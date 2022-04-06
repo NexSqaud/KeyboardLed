9fb379abeda33bd0f2b0fd8ea6694e5f403c1b78
NexSqaud <tf2g3124@gmail.com>
2022-04-06 15:57:28 +0700

    Add project files.

---
+++ KeyboardLed/KeyboardController.cs
@@ -0,0 +1,204 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace KeyboardLed
+{
+    internal class KeyboardController
+    {
+        private const string TestSequence = "5";
+        private const string ResultSquence = "ok!";
+
+        private const string UpdateSquence = "6";
+
+        private Color[][] _pixelsBuffer = new Color[][] 
+        {
+            new Color[16],
+            new Color[17],
+            new Color[17],
+            new Color[13],
+            new Color[13],
+            new Color[11]
+        };
+
+        private SerialPort _port;
+
+        public KeyboardController()
+        {
+            var ports = SerialPort.GetPortNames();
+
+            for(int i = 2; i < ports.Length; i++)
+            {
+                try
+                {
+                    var port = new SerialPort(ports[i], 115200);
+                    port.RtsEnable = true;
+                    port.DtrEnable = true;
+                    port.ReadTimeout = 2000;
+                    port.NewLine = "\n";
+                    port.Open();
+                    port.Write(TestSequence);
+                    var result = port.ReadLine();
+                    if(result == ResultSquence)
+                    {
+                        _port = port;
+                        break;
+                    }
+                    port.Close();
+                }
+                catch(Exception)
+                {
+
+                }
+            }
+
+            if(_port == null)
+            {
+                throw new Exception("Port not found");
+            }
+
+        }
+
+        public void SetPixelColor(int line, int pixel, Color color)
+        {
+            if(line >= _pixelsBuffer.Length || line < 0)
+            {
+                return;
+            }
+
+            if (pixel >= _pixelsBuffer[line].Length || pixel < 0)
+            {
+                return;
+            }
+
+            _pixelsBuffer[line][pixel] = color;
+        }
+
+        public void SetLineColor(int line, Color color)
+        {
+            if (line >= _pixelsBuffer.Length || line < 0)
+            {
+                return;
+            }
+
+            for(int i = 0; i < _pixelsBuffer[line].Length; i++)
+            {
+                _pixelsBuffer[line][i] = color;
+            }
+        }
+
+        public void Fill(Color color)
+        {
+            for (int i = 0; i < _pixelsBuffer.Length; i++)
+            {
+                for(int j = 0; j < _pixelsBuffer[i].Length; j++)
+                {
+                    _pixelsBuffer[i][j] = color;
+                }
+            }
+        }
+
+        public void PushLineColor(int line, Color color)
+        {
+            var buffer = new byte[] {(byte)'0', (byte)line, color.Red, color.Green, color.Blue };
+            WriteToPort(buffer);
+        }
+
+        public void PushBuffer()
+        {
+            var buffer = new List<byte>();
+            buffer.Add((byte)'1');
+
+            var i = 0;
+            var j = 0;
+            var counter = 0;
+
+            while(true)
+            {
+                if(j == _pixelsBuffer[i].Length)
+                {
+                    i++;
+                    j = 0;
+                }
+
+                if (_pixelsBuffer[i].Length > j)
+                {
+                    buffer.AddRange(new byte[] { (byte)i, (byte)j, _pixelsBuffer[i][j].Red, _pixelsBuffer[i][j].Green, _pixelsBuffer[i][j].Blue });
+                }
+
+                if((counter % 8) == 0 && counter != 0)
+                {
+                    var arrayBuffer = buffer.ToArray();
+                    WriteToPort(arrayBuffer);
+                    buffer.RemoveRange(1, buffer.Count - 1);
+                    Thread.Sleep(1);
+                }
+
+                if(i == _pixelsBuffer.Length - 1 && j == _pixelsBuffer[i].Length - 1)
+                {
+                    var arrayBuffer = buffer.ToArray();
+                    WriteToPort(arrayBuffer);
+                    break;
+                }
+
+
+                j++;
+                counter++;
+            }
+
+            Thread.Sleep(1);
+
+            _port.Write(UpdateSquence);
+        }
+
+        public void PushFillColor(Color color)
+        {
+            WriteToPort((byte)'2', color.Red, color.Green, color.Blue);
+        }
+
+        public void PushClear()
+        {
+            WriteToPort((byte)'3');
+        }
+
+        private void WriteToPort(params byte[] bytes)
+        {
+            if(!_port.IsOpen)
+            {
+                try
+                {
+                    _port.Open();
+                }
+                catch(FileNotFoundException)
+                {
+                    MessageBox.Show("Keyboard disconnected!", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                    throw;
+                }
+            }
+
+            try
+            {
+                _port.Write(bytes, 0, bytes.Length);
+            }
+            catch(InvalidOperationException)
+            {
+                MessageBox.Show("Keyboard disconnected!", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                throw;
+            }
+        }
+    }
+
+    struct Color
+    {
+        public byte Red;
+        public byte Green;
+        public byte Blue;
+    }
+
+}
---
+++ KeyboardLed/MainWindow.xaml.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace KeyboardLed
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        private KeyboardController _controller = new();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void KeyMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (!PixelColorPicker.SelectedColor.HasValue)
+            {
+                return;
+            }
+
+            var rectangle = (Rectangle)sender;
+            var lineIndex = int.Parse(rectangle.Name[1..2]);
+            var pixelIndex = int.Parse(rectangle.Name[3..]);
+
+            var color = PixelColorPicker.SelectedColor.Value;
+            rectangle.Fill = new SolidColorBrush(color);
+
+            _controller.SetPixelColor(lineIndex, pixelIndex, new Color()
+            {
+                Red = color.R,
+                Green = color.G,
+                Blue = color.B
+            });
+        }
+
+        private void RowFillClick(object sender, RoutedEventArgs e)
+        {
+            if (!PixelColorPicker.SelectedColor.HasValue)
+            {
+                return;
+            }
+
+            var button = (Button)sender;
+            var lineIndex = int.Parse(button.Name[1..]);
+            var color = PixelColorPicker.SelectedColor.Value;
+
+            int i = 0;
+            while(true)
+            {
+                var key = FindName($"K{lineIndex}_{i}");
+                if(key == null)
+                {
+                    break;
+                }
+                ((Rectangle)key).Fill = new SolidColorBrush(color);
+                i++;
+            }
+
+            _controller.SetLineColor(lineIndex, new Color
+            {
+                Red = color.R,
+                Green = color.G,
+                Blue = color.B
+            });
+        }
+        private void FillButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (!PixelColorPicker.SelectedColor.HasValue)
+            {
+                return;
+            }
+            var color = PixelColorPicker.SelectedColor.Value;
+
+            int i = 0;
+            int j = 0;
+            while(true)
+            {
+                var key = FindName($"K{i}_{j}");
+                if(key == null)
+                {
+                    if(j == 0)
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        i++;
+                        j = 0;
+                    }
+                    continue;
+                }
+                ((Rectangle)key).Fill = new SolidColorBrush(color);
+                j++;
+            }
+
+            _controller.Fill(new Color
+            {
+                Red = color.R,
+                Green = color.G,
+                Blue = color.B
+            });
+        }
+
+        private void PushButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                _controller.PushBuffer();
+            }
+            catch(Exception)
+            {
+
+            }
+        }
+
+    }
+}
